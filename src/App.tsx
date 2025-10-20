84c4d3d8daa34bc943bc1b2591a86112d868f47e
Paola Quintanilla <74014.linares@gmail.com>
2025-10-20 00:11:00 -0400

    starting working in projects area

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/Sections/HeroSection";
+import ProjectsSection from "./components/Sections/ProjectsSection";
 import { SkillsSection } from "./components/Sections/SkillsSection";
 import { ThemeProvider } from "./context/ThemeProvider";
 
@@ -10,6 +11,7 @@ const App = () => {
         <Navbar />
         <HeroSection />
         <SkillsSection />
+        <ProjectsSection />
       </div>
     </ThemeProvider>
   );
